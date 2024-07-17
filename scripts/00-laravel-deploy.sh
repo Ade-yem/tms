a55a95a4f3f9f087016542b4a9aabbdc28d8073a
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2024-07-17 09:36:00 +0100

    Update permissions for storage directory in Laravel deploy script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -5,10 +5,10 @@ chown -R www-data:www-data /var/www/html/storage
 chown -R www-data:www-data /var/www/html/bootstrap/cache
 chown -R www-data:www-data /var/www/html/storage/logs
 
-chmod 775 /var/www/html/storage
-chmod 775 /var/www/html/storage/logs
+chmod 777 /var/www/html/storage
+chmod 777 /var/www/html/storage/logs
 
-chmod 775 /var/www/html/bootstrap/cache
+chmod 777 /var/www/html/bootstrap/cache
 
 composer install --no-dev --working-dir=/var/www/html
 
